7502a6349ebe492c9a48e1063eaec33e6ead0d53
Russell Hoyer <RussellHoyer88@gmail.com>
2023-06-13 11:51:23 -0400

    wip

---
+++ WindemereManor.Models/FridgeItem.cs
@@ -0,0 +1,14 @@
+﻿namespace WindemereManor.Models
+{
+    public class FridgeItem
+    {
+        public FridgeItem()
+        {
+            ItemName = "Fridge Item";
+        }
+
+        public string ItemName { get; set; }
+        public DateOnly Expiration { get; set; }
+        public DateOnly AddedOn { get; set; }
+    }
+}
